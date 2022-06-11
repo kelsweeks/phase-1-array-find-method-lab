cb9cd4b8b8bffec54d2c231ce843402c1e617f84
Kelsey Weeks <kelsey@kelseyweeks.com>
2022-06-11 08:54:20 -0500

    commit tree d44506d74dd7d76f850dbfa973e196aafbc9f4d5

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+// we need to write a function
+//receive an array of objects as arguement
+// using dot notation .find()
+// in bracket notation means objects
+
+function superbowlWin(array) {
+    let winningYear = array.find((array) => array.result === "W");
+    if (winningYear) {
+      return winningYear.year;
+    }
+  }
+  superbowlWin(record);
